dfed4588169a000395696e9c6b2fa477b59db84d
kulichanskiy <kulichanskiy.js@gmail.com>
2025-01-24 19:07:35 -0800

    Icons changed to svg
    Footer update

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,7 +1,22 @@
-document.getElementById('like').addEventListener('click', () => {
-    alert('Вы понравились этому человеку!');
-});
+import { switchTab } from "./tab_switch.js";
 
-document.getElementById('dislike').addEventListener('click', () => {
-    alert('Вы отклонили этого человека.');
+const homeTabBtn = document.getElementById(".home-tab-btn");
+const likesTabBtn = document.getElementById(".likes-tab-btn");
+const eventsTabBtn = document.getElementById(".events-tab-btn");
+const newsTabBtn = document.getElementById(".news-tab-btn");
+const profileTabBtn = document.getElementById(".profile-tab-btn");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".footer-button");
+    const slider = document.querySelector(".footer-slider");
+
+    buttons.forEach((button, index) => {
+        const moveSlider = () => {
+            slider.style.left = `${index * 20}%`;
+        };
+
+        // Добавляем обработчики для мыши и сенсора
+        button.addEventListener("mouseenter", moveSlider);
+        button.addEventListener("touchstart", moveSlider);
+    });
 });
